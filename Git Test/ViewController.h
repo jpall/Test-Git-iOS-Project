e6f9561a0767ead19ca8fe911dad27d502ff95e3
Juri Pall <pallju@JPalls-MacBook-Air.local>
2015-02-10 22:54:59 +0100

    Initial Commit

---
+++ Git Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Git Test
+//
+//  Created by Juri Pall on 10.02.15.
+//  Copyright (c) 2015 Juri Pall. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Git Test/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Git Test
+//
+//  Created by Juri Pall on 10.02.15.
+//  Copyright (c) 2015 Juri Pall. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
