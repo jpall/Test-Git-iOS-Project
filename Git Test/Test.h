9cccd952775b976e77f227a63981ff0f01a3a6e7
Juri Pall <pallju@JPalls-MacBook-Air.local>
2015-02-11 20:53:53 +0100

    added a test class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Juri Pall on 11.02.15.
+//  Copyright (c) 2015 Juri Pall. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Juri Pall on 11.02.15.
+//  Copyright (c) 2015 Juri Pall. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
